e915013048f97a3c3848cd989d6cc3a8fdfcd48f
xerdin442 <mudianthonio27@gmail.com>
2024-07-26 09:47:28 +0100

    fixed routing

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 import auth from './auth';
 import user from './user';
+import features from './features';
 
 const router = express.Router()
 
 export default (): express.Router => {
   auth(router)
   user(router)
+  features(router)
   
   return router;
 }
