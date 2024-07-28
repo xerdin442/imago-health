e0d77742968d738526fa974f5f4b164db65b407b
xerdin442 <mudianthonio27@gmail.com>
2024-07-28 07:25:21 +0100

    correcting multer config

--- server/src/config/storage.ts
+++ server/src/config/storage.ts
@@ -12,35 +12,12 @@ cloudinary.config({
 });
 
 export const upload = (folderName: string) => {
-  const audioMimetypes: string[] = ['audio/mpeg',  // MP3
-  'audio/mp3',
-  'audio/wav',
-  'audio/x-wav',
-  'audio/aiff',
-  'audio/x-aiff',
-  'audio/aac',
-  'audio/x-aac',
-  'audio/ogg',
-  'audio/flac',
-  'audio/x-flac',
-  'audio/mp4',
-  'audio/x-m4a']
   const imageMimetypes: string[] = ['image/png', 'image/jpg', 'image/jpeg']
 
-  const storage = new CloudinaryStorage({
+  const cloudinaryStorage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: (req, file) => {
-      console.log(file.path)
       const public_id = new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname;
-      
-      if (audioMimetypes.includes(file.mimetype)) {
-        return {
-          folder: folderName,
-          public_id,
-          resource_type: 'video',
-          bitrate: 32000 // Adjust the bitrate of the audio file to enhance processing
-        };
-      }
       
       return {
         folder: folderName,
@@ -54,7 +31,7 @@ export const upload = (folderName: string) => {
   });
 
   const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    if (audioMimetypes.includes(file.mimetype) || imageMimetypes.includes(file.mimetype)) {
+    if (imageMimetypes.includes(file.mimetype)) {
       cb(null, true);
     } else {
       cb(null, false);
@@ -62,11 +39,39 @@ export const upload = (folderName: string) => {
   };
 
   return multer({
-    storage,
+    storage: cloudinaryStorage,
     limits: { fieldSize: 5 * 1024 * 1024 }, // Image sizes must be less than 5MB
     fileFilter
   });
 };
+
+export const uploadAudio = () => {
+  const audioMimetypes: string[] = ['audio/mpeg', 'audio/mp3', 'audio/wav', 'audio/x-wav', 'audio/aiff', 'audio/x-aiff', 'audio/aac', 'audio/x-aac',
+    'audio/ogg', 'audio/flac', 'audio/x-flac', 'audio/mp4', 'audio/x-m4a']
+
+  const audioStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'audio/')
+    },
+    filename: (req, file, cb) => {
+      cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
+    }
+  })
+
+  const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+    if (audioMimetypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+  return multer({
+    storage: audioStorage,
+    limits: { fieldSize: 5 * 1024 * 1024 }, // Image sizes must be less than 5MB
+    fileFilter
+  });
+}
 
 export const deleteUpload = (publicId: string) => {
   cloudinary.uploader.destroy(publicId, (error, result) => {
--- server/src/routes/features.ts
+++ server/src/routes/features.ts
@@ -2,17 +2,10 @@ import express from 'express';
 
 import * as Feature from '../controllers/features';
 import { isLoggedIn } from '../middlewares/authorization';
-import { upload } from '../config/storage';
+import { uploadAudio } from '../config/storage';
 
 export default (router: express.Router) => {
   router.get('/chats/:chatId', isLoggedIn, Feature.getChatHistory)
   router.post('/launch-assistant', isLoggedIn, Feature.launchHealthAssistant)
-  
-  router.post('/ask-health-assistant/:chatId', upload('gemini-folder').single('symptomAudio'), (req, res, next) => {
-    console.log('Middleware reached');
-    console.log('File:', req.file);
-    console.log('Body:', req.body);
-    next();
-  }, isLoggedIn, Feature.healthAssistant);
-  
+  router.post('/ask-health-assistant/:chatId', uploadAudio().single('symptomAudio'), isLoggedIn, Feature.healthAssistant);
 }
