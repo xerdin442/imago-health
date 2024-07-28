3d76d0b8448a622ec2016c3398cf666748c61768
xerdin442 <mudianthonio27@gmail.com>
2024-07-28 06:42:14 +0100

    more changes

--- server/src/routes/features.ts
+++ server/src/routes/features.ts
@@ -7,5 +7,12 @@ import { upload } from '../config/storage';
 export default (router: express.Router) => {
   router.get('/chats/:chatId', isLoggedIn, Feature.getChatHistory)
   router.post('/launch-assistant', isLoggedIn, Feature.launchHealthAssistant)
-  router.post('/ask-health-assistant/:chatId', upload('gemini-folder').single('symptomAudio'), isLoggedIn, Feature.healthAssistant)
+  
+  router.post('/ask-health-assistant/:chatId', upload('gemini-folder').single('symptomAudio'), (req, res, next) => {
+    console.log('Middleware reached');
+    console.log('File:', req.file);
+    console.log('Body:', req.body);
+    next();
+  }, isLoggedIn, Feature.healthAssistant);
+  
 }
