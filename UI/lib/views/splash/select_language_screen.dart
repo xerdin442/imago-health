e04be229410f87952fb0bd1f7370f4dd8a28fb2c
kingstech000 <kingstech0001@gmail.com>
2024-08-12 23:55:10 +0100

    last one

--- UI/lib/views/splash/select_language_screen.dart
+++ UI/lib/views/splash/select_language_screen.dart
@@ -10,36 +10,45 @@ class LanguageSwitcher extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              color: appColorLightGrey,
-              borderRadius: BorderRadius.all(Radius.circular(10.r))),
-          height: 70.h,
-          width: 250.w,
-          child: Center(
-            child: DropdownButton<Locale>(
-              value: Provider.of<LocalizationProvider>(context).locale,
-              icon: const Icon(Icons.language),
-              onChanged: (Locale? newLocale) {
-                if (newLocale != null) {
-                  Provider.of<LocalizationProvider>(context, listen: false)
-                      .setLocale(newLocale);
-                  Navigator.pushNamed(context, 'carousel');
-                }
-              },
-              items: L10n.all.map<DropdownMenuItem<Locale>>((Locale locale) {
-                return DropdownMenuItem<Locale>(
-                  value: locale,
-                  child: Text(
-                    L10n.getFlag(locale.languageCode),
-                    style: TextStyle(fontSize: 20.sp),
-                  ),
-                );
-              }).toList(),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: appColorLightGrey,
+                  borderRadius: BorderRadius.all(Radius.circular(10.r))),
+              height: 70.h,
+              width: 250.w,
+              child: Center(
+                child: DropdownButton<Locale>(
+                  value: Provider.of<LocalizationProvider>(context).locale,
+                  icon: const Icon(Icons.language),
+                  onChanged: (Locale? newLocale) {
+                    if (newLocale != null) {
+                      Provider.of<LocalizationProvider>(context, listen: false)
+                          .setLocale(newLocale);
+                      Navigator.pushNamed(context, 'carousel');
+                    }
+                  },
+                  items:
+                      L10n.all.map<DropdownMenuItem<Locale>>((Locale locale) {
+                    return DropdownMenuItem<Locale>(
+                      value: locale,
+                      child: Text(
+                        L10n.getFlag(locale.languageCode),
+                        style: TextStyle(fontSize: 20.sp),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
             ),
           ),
-        ),
+          Text(
+            "Note some part of the app may remain fundamentally in English",
+            style: TextStyle(color: appColorDarkPurple, fontSize: 15.sp),
+          )
+        ],
       ),
     );
   }
